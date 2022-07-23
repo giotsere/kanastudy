b962e4549f2d0c1a0e2cead8549d4f9318bbfa92
giotsere <40203852+giotsere@users.noreply.github.com>
2022-07-23 20:35:08 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,6 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-slate-800 text-white w-fit m-auto text-center pb-6">
-      <p className="mb-4">
-        Made by
-        <a
-          href="https://www.github.com/giotsere"
-          target="_blank"
-          className="footer_p"
-        >
-          Giorgi Tsereteli
-        </a>
-      </p>
-
       <a
         href="https://www.github.com/giotsere/kanastudy"
         target="_blank"
